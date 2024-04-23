3d86e099ffddb17b37d283067304ca536660708c
Behçet <behcetmuhammed>
2024-04-23 21:33:37 +0300

    Initial commit

---
+++ src/Week1/Main.java
@@ -0,0 +1,44 @@
+package Week1;
+import java.util.Scanner;
+/*
+Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav
+puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+*************
+Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması
+60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+Not: Ternary Operator ya da Switch Case kullanarak yapılacak.
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik sonucunu giriniz: ");
+        double Matematik = input.nextDouble();
+        System.out.println("Fizik sonucunu giriniz: ");
+        double Fizik = input.nextDouble();
+        System.out.println("Kimya sonucunu giriniz: ");
+        double Kimya = input.nextDouble();
+        System.out.println("Türkçe sonucunu giriniz: ");
+        double Türkçe = input.nextDouble();
+        System.out.println("Tarih sonucunu giriniz: ");
+        double Tarih = input.nextDouble();
+        System.out.println("Müzik sonucunu giriniz: ");
+        double Müzik = input.nextDouble();
+
+        double ortalama = (Matematik + Kimya + Fizik + Türkçe + Tarih + Müzik) / 6;
+
+        System.out.println("Not Ortalamanız: " + ortalama);
+
+            /*
+            if (ortalama >= 60){
+                System.out.println("Sınıfı geçti");
+            }
+            else{
+                System.out.println("Sınıfta Kaldı");
+            }
+             */
+        //veya
+        String durum = (ortalama >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(durum);
+    }
+}
