98e0aa9c6ab8712d7ba4673d8131e31f9e5db398
Behçet <behcetmuhammed>
2024-04-24 00:17:48 +0300

    Taksimetre Ödevi Yapıldı

---
+++ src/Week1/taksimetre.java
@@ -0,0 +1,26 @@
+package Week1;
+/*
+Taksimetre Programı
+Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
+
+Taksimetre KM başına 2.20 TL tutmaktadır.
+Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+Taksimetre açılış ücreti 10 TL'dir.
+*/
+import java.util.Scanner;
+public class taksimetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Gidilen KM'yi Giriniz: ");
+        double km = input.nextDouble();
+        double startPrice= 10, priceKm = 2.20 ,price = km * priceKm;
+        if (price >= 20) {
+            price = startPrice + (km * 2.20);
+        }
+        else {
+            price = 20;
+        }
+        String priceV = String.format("%.0f", price);
+        System.out.println("Taksimetre Tutarı: " + priceV);
+    }
+}
