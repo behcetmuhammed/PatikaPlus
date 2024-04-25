24a7ee2ed625cbd936a4f7a277fcf857d4f35a36
Behçet <behcetmuhammed>
2024-04-25 23:10:28 +0300

    Sorting Number Homework Done - updated

--- src/Week1/Sorting.java
+++ src/Week1/Sorting.java
@@ -20,22 +20,22 @@ public class Sorting {
 
         if (sayi1 < sayi2 && sayi1 < sayi3) {
             if (sayi2 < sayi3) {
-                System.out.print("Sıralama: " + sayi1 + " < " + sayi2 + " < " + sayi3);
+                System.out.print("Sıralama: " + sayi1 + " => " + sayi2 + " => " + sayi3);
             } else {
-                System.out.print("Sıralama: " + sayi1 + " < " + sayi3 + " < " + sayi2);
+                System.out.print("Sıralama: " + sayi1 + " => " + sayi3 + " => " + sayi2);
             }
         } else if (sayi2 < sayi1 && sayi2 < sayi3) {
             if (sayi1 < sayi3) {
-                System.out.print("Sıralama: " + sayi2 + " < " + sayi1 + " < " + sayi3);
+                System.out.print("Sıralama: " + sayi2 + " => " + sayi1 + " => " + sayi3);
             } else {
-                System.out.print("Sıralama: " + sayi2 + " < " + sayi3 + " < " + sayi1);
+                System.out.print("Sıralama: " + sayi2 + " => " + sayi3 + " => " + sayi1);
             }
 
         } else {
             if (sayi1 < sayi2) {
-                System.out.print("Sıralama: " + sayi3 + " < " + sayi1 + " < " + sayi2);
+                System.out.print("Sıralama: " + sayi3 + " => " + sayi1 + " => " + sayi2);
             } else {
-                System.out.print("Sıralama: " + sayi3 + " < " + sayi2 + " < " + sayi1);
+                System.out.print("Sıralama: " + sayi3 + " => " + sayi2 + " => " + sayi1);
             }
         }
         {
