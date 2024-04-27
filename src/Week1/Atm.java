8b9974fe53aafeea9f4836f1f7c0115fac9314a2
Behçet <behcetmuhammed>
2024-04-28 00:46:54 +0300

    ATM Homework Done Updated

--- src/Week1/Atm.java
+++ src/Week1/Atm.java
@@ -2,6 +2,10 @@ package Week1;
 
 import java.util.Scanner;
 
+/*
+    ##Ödev
+    Aynı projedeki ATM işlemlerini "Switch-Case" kullanarak yapınız.
+*/
 public class Atm {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -13,7 +17,7 @@ public class Atm {
 
         while (sifreHakki != 0) {
             sifreHakki--;
-            System.out.print("Lütfen kullanıcı adını giriniz: ");
+            System.out.print("\nLütfen kullanıcı adını giriniz: ");
             kullaniciAdi = input.next();
             System.out.print("Lütfen şifrenizi giriniz: ");
             kullaniciSifresi = input.next();
@@ -25,25 +29,29 @@ public class Atm {
                     System.out.println(" 1- Para çek\n 2- Para yatır \n 3- Bakiye sorgula \n 4- Çıkış yap");
                     System.out.print("Hoşgeldiniz. Lütfen işlem türünü seçiniz: ");
                     secim = input.nextInt();
-                    if(secim==1){
-                        System.out.println("Çekilecek miktarı girin: ");
-                        int cekilenPara = input.nextInt();
+                    switch (secim) {
+                        case 1:
+                            System.out.print("Çekilecek miktarı girin: ");
+                            int cekilenPara = input.nextInt();
 
-                        if(cekilenPara<bakiye) {
-                            bakiye -= cekilenPara;
-                            System.out.println("İşlem başarılı. " + cekilenPara + " TL çekildi. Güncel bakiyen: " + bakiye + " TL");
-                        }else{
-                            System.out.println("Bakiyeniz yetersiz");
+                            if (cekilenPara < bakiye) {
+                                bakiye -= cekilenPara;
+                                System.out.println("İşlem başarılı. " + cekilenPara + " TL çekildi. Güncel bakiyen: " + bakiye + " TL");
+                            } else {
+                                System.out.println("Bakiyeniz yetersiz");
+                                break;
+                            }
                             break;
-                        }
-                    }
-                    else if(secim==2){
-                        System.out.println("Yatıralacak miktarı girin: ");
-                        int yatirilanPara = input.nextInt();
-                        bakiye += yatirilanPara;
-                        System.out.println("İşlem başarılı. " + yatirilanPara + " TL yatırıldı. Güncel bakiyen: " +  bakiye+ " TL");
-                    }else if(secim==3){
-                        System.out.println("Bakiyeniz: " + bakiye + " TL");
+                        case 2:
+                            System.out.print("Yatıralacak miktarı girin: ");
+                            int yatirilanPara = input.nextInt();
+                            bakiye += yatirilanPara;
+                            System.out.println("İşlem başarılı. " + yatirilanPara + " TL yatırıldı. Güncel bakiyen: " + bakiye + " TL");
+                            break;
+
+                        case 3:
+                            System.out.println("Bakiyeniz: " + bakiye + " TL");
+                            break;
                     }
                 } while (secim != 4);
                 System.out.println("Çıkış yapıldı.");
@@ -51,8 +59,8 @@ public class Atm {
 
             } else {
                 System.out.println("Yanlış şifre! Kalan hakkınız: " + sifreHakki);
-                if(sifreHakki == 0){
-                    System.out.println("Çok fazla yanlış şifre girdiniz. Sistem bloke oldu.");
+                if (sifreHakki == 0) {
+                    System.out.println("\nÇok fazla yanlış şifre denemesi!. Sistem bloke oldu.");
                 }
             }
         }
