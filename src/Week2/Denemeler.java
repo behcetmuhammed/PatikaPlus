3a85937f217f36d5d89c87e4667c4137f47bb408
Behçet <behcetmuhammed>
2024-04-30 23:45:38 +0300

    xx

--- src/Week2/Denemeler.java
+++ src/Week2/Denemeler.java
@@ -2,12 +2,7 @@ package Week2;
 
 public class Denemeler {
     public static void main(String[] args) {
-        int sayi = 5;
 
-        int fact = 1;
-        for (int i = sayi; i > 0; i--) {
-            fact *= i;
-        }
-        System.out.println("Faktöriyel sonucu: " + fact);
     }
 }
+
