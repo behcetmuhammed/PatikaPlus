016f33e5c94e46076957dbd0ce17c4906d96d1cc
Behçet <behcetmuhammed>
2024-05-30 01:35:39 +0300

    SQL-11

--- src/Week6/SQL_Homeworks/odev11.sql
+++ src/Week6/SQL_Homeworks/odev11.sql
@@ -38,12 +38,20 @@ EXCEPT
 
 
 -- İlk 3 sorguyu tekrar eden veriler için de yapalım.
-(
-	SELECT first_name
-	FROM actor
-)
-UNION ALL
-(
-	SELECT first_name
-	FROM customer
-);
+(SELECT first_name
+FROM actor)
+UNION ALL --tekrar edenleri de verir
+(SELECT first_name
+FROM customer);
+
+(SELECT first_name
+FROM actor)
+INTERSECT ALL --
+(SELECT first_name
+FROM customer);
+
+(SELECT first_name
+FROM actor)
+EXCEPT ALL
+(SELECT first_name
+FROM customer);
